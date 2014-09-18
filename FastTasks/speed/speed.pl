7585c7dd6c8a063b90775dae4b4dc3f4388ed6b5
daniellazarov <daniel.r.lazarov@gmail.com>
2014-09-18 12:40:58 +0300

    fast task speed

--- FastTasks/speed/speed.pl
+++ FastTasks/speed/speed.pl
@@ -12,53 +12,42 @@ my $maxlength = 0;
 
 sub searchPath
 {
-    my ($start, $target, $arrayRef,$min,$max, $visitedRef, $st) = @_;
+    my ($start, $target, $arrayRef,$min,$max, $visitedRef, $st, $inner) = @_;
     my @array = @{$arrayRef};
     my @vis = @{$visitedRef};
-    my $true = 0;
-    if($array[$start][$target])
-    {
-        my @arr = @{$array[$start][$target]};
-        for (my $var = 0; $var <= $#arr; $var++) 
+    $vis[$start] = 1;
+
+    for (my $i = 0; $i < $n; $i++) {
+        if($i == $target && $array[$start][$i] && !$vis[$i])
         {
-            if($arr[$var] >= $min && $arr[$var] <= $max)
+            my @arr = @{$array[$start][$i]};
+            for (my $var = 0; $var <= $#arr; $var++) 
             {
-                if($min == 3 && $max == 7 && $st == 0)
+                if($arr[$var] >= $min && $arr[$var] <= $max)
                 {
-                    # print "FOUND! start: $st  Current: $start  Target: $target  Value: " . $arr[$var] . "\n";
-                }
-                $true = 1;
-            }  
-        } 
-    }
-    if($true)
-    {
-        return 1;
-    }
-    else
-    {
-        for (my $i = 0; $i < $m; $i++) {
-            if($array[$start][$i] && !$vis[$start][$i])
-            {
-                my @arr = @{$array[$start][$i]};
-                for (my $var = 0; $var <= $#arr; $var++) {
-                    if($min == 3 && $max == 7 && $st == 0 && $i!= $st)
-                    {
-                        print "Possible Current: $start  Target: $target Next: $i  Value: " . $arr[$var] . "\n";
-                    }
-                    if($arr[$var] >= $min && $arr[$var] <= $max && $i!= $st)
-                    {
-                        $vis[$start][$i] = 1;
-                        return searchPath($i, $target, \@array, $min, $max, \@vis, $st);
-                    }
-                    elsif($min == 3 && $max == 7 && $st == 0 && $i!= $st)
-                    {
-                         print "Missed   current: ".$start . "  target: " . $target . " next: " . $i . "  path: ". $arr[$var] , " min:$min max:$max\n";
-                    } 
+                    return 1;
                 }
             }
         }
     }
+    my $found = 0;
+    for (my $i = 0; $i < $n; $i++) {
+        if($array[$start][$i] && !$vis[$i])
+        {
+            my @arr = @{$array[$start][$i]};
+            for (my $var = 0; $var <= $#arr; $var++) 
+            {
+                if($arr[$var] >= $min && $arr[$var] <= $max)
+                {
+                    if(searchPath($i, $target, \@array, $min, $max, \@vis, $st, 1))
+                    {
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
 }
 
 my @main;
@@ -97,22 +86,24 @@ for (my $max = 1; $max <= $maxlength; $max++) {
     for (my $min = 1; $min <= $max; $min++) {
         my $valid = 1;
         for (my $i = 0; $i < $n; $i++) {
-            for (my $j = $i+1; $j < $n; $j++) {
+            for (my $j = $i; $j < $n; $j++) {
                 if($i!=$j)
                 {
                     my @visited;
-                    if(!searchPath($i,$j,\@main, $min, $max, \@visited, $i))
+                    if(!searchPath($i,$j,\@main, $min, $max, \@visited, $i, 0))
                     {
-                        
-                        if($min == 3 && $max == 7){
-                             print "impossible for $i $j $min $max\n";
-                        }
                         $valid = 0;
                     }
                 }
             }
         }
-        if($valid && $max-$min <= $minDif)
+        if($valid && $max-$min < $minDif)
+        {
+            $optMin = $min;
+            $optMax = $max;
+            $minDif = $max - $min;
+        }
+        elsif($valid && $max-$min == $minDif)
         {
             if($min < $optMin)
             {
@@ -122,17 +113,5 @@ for (my $max = 1; $max <= $maxlength; $max++) {
         }
     }
 }
-# for (my $i = 0; $i < $m; $i++) {
-#     for (my $j = 0; $j < $m; $j++) {
-#         print $i , ", " , $j , "--------------\n";
-#         if($main[$i][$j])
-#         {
-#             my @arr = @{$main[$i][$j]};
-#             for (my $e = 0; $e <= $#arr; $e++) {
-#                 print $arr[$e] , "\n";
-#             }
-#         }
-#     }
-# }
 
-print $optMin , " ", $optMax , "\n";
+print "Min: " . $optMin . "  Max: ". $optMax , "\n";
