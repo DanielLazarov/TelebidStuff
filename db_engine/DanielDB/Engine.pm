a369c6866a347758714ea4d3c4fba66b0fc73188
daniellazarov <daniel.r.lazarov@gmail.com>
2015-03-06 21:57:14 +0200

    lala

--- db_engine/DanielDB/Engine.pm
+++ db_engine/DanielDB/Engine.pm
@@ -41,166 +41,12 @@ sub connect($$)
     }
 }
 
-sub Insert()#TODO insert_hash may be arr_ref
-{
-    my ($self, $table_name, $insert_hash) = @_;
-
-    my $fh;
-    if(!-f $$self{db_dir}. "/$table_name")
-    {
-        die "Not Existing Table";
-    }
-    open($fh, "+>>" . $$self{db_dir} . "/$table_name") or die $!;
-
-
-    my $arr_ref = ReadTableHeader($fh);
-    
-    my @columns_arr = @{$arr_ref};
-
-    my $col_count = scalar @columns_arr;
-
-    WriteRowHeading($fh);
-
-    foreach my $column(@columns_arr)
-    {
-        $$column{write}->($fh, $$insert_hash{$$column{name}});
-    }
-    
-    close($fh);
-
-
-}
-sub Select()
-{
-    my ($self, $table_name, $select_hash) = @_;
-
-    my $fh;
-
-    if(!-f $$self{db_dir} . "/$table_name")
-    {
-        die "Not Existing Table";
-    }
-    open($fh, "<", $$self{db_dir} . "/$table_name") or die $!;
-    
-    my $arr_ref = ReadTableHeader($fh);
-
-    my @columns_arr = @{$arr_ref};
-    my @colnames;
-    my @result;
-
-    foreach my $column(@columns_arr) #get Colnames
-    {
-        push @colnames, $$column{name};
-    }
-    push @result, \@colnames;
-
-    while(!eof($fh))
-    {
-        my @row;
-        my $is_valid = 1;#TODO conditions!+
-        foreach my $column(@columns_arr)
-        {
-            my $val = $$column{read}->($fh);
-            #TODO do the checking
-            if(!$is_valid)
-            {
-                last;
-            }
-            push @row, $val;
-        }
-        if($is_valid)
-        {
-            push @result, \@row;
-        }
-    }
-    return \@result;
-}
-
-sub WriteRowHeading($;$)
-{
-    my($fh, $params) = @_;
-
-    my $flags = 0;
-
-    if(defined $$params{deleted})
-    {
-        $flags += 128;
-    }
-
-    print $fh(pack("C",$flags );
-}
-sub Update()
-{
-    my ($self, $table_name, $update_hash) = @_;
-
-    my $fh;
-
-    if(!-f $$self{db_dir} . "/$table_name")
-    {
-        die "Not Existing Table";
-    }
-    open($fh, "+>>", $$self{db_dir} . "/$table_name") or die $!;
-    
-    my $arr_ref = ReadTableHeader($fh);
-
-    my @columns_arr = @{$arr_ref};
-    my @colnames;
-    foreach my $column(@columns_arr)
-    {
-        push @colnames, $$column{name};
-    }
-
-    while(!eof($fh))
-    {  
-        my @row;
-        my $row_beginning_pos = tell($fh);
-        my $is_valid = 1;
-
-        foreach my $column(@columns_arr)
-        {
-            my $val = $$column{read}->($fh);
-            #TODO do the checking
-            if(!$is_valid)
-            {
-                last;
-            }
-            push @row, $val;
-        }
-        
-        if($is_valid)
-        {
-            my $cols_count = scalar(@columns_arr);
-            my $update_row;
-
-            for(my $i = 0; $i < $cols_count; $i++)
-            {
-                $$update_row{$columns_arr[$i]{name}} = exists ($$update_hash{$columns_arr[$i]{name}}) ?  $$update_hash{$columns_arr[$i]{name}} : $row[$i]; #TODO chec    
-            }
-
-            seek($fh, $row_beginning_pos, 0);
-
-            
-            foreach my $column(@columns_arr)
-            {
-                $$column{write}->($fh, $$update_row{$$column{name}});
-            }   
-        }
-
-
-        
-        #TODO read row
-        #validate from given cryteria
-        #create updated row (combinatian from read row and updated fields from input)
-        #go back to $row_beginning_pos
-        #write new info
-    }
-}
 sub CreateDB()
 {
     my ($self) = @_;
 }
 
-sub CreateTable()
+sub CreateTable($$$)
 {
     my ($self, $table_name, $columns) = @_;
     
@@ -213,7 +59,8 @@ sub CreateTable()
     open($fh, ">" . $$self{db_dir} . "/$table_name") or die   $$self{db_dir} . "/$table_name" .$!;
 
     my @columns_arr = @{$columns};
-    
+
+    print $fh pack("I", 0);
     print $fh pack("C", scalar @columns_arr); #column count
 
     foreach my $column(@columns_arr)
@@ -225,7 +72,7 @@ sub CreateTable()
     close($fh);
 }
 
-sub ReadTableHeader()
+sub ReadTableMeta($)
 {
     my ($fh) = @_;
 
@@ -234,6 +81,10 @@ sub ReadTableHeader()
     seek $fh,0,0; #set fh at beginning
 
     my $buffer = "";
+
+    read($fh, $buffer, $Config{intsize});
+    my $row_count = unpack("I", $buffer);
+
     read($fh, $buffer, 1);
     my $col_count = unpack("C", $buffer);
     
@@ -254,10 +105,196 @@ sub ReadTableHeader()
         push @col_arr, $column;    
     }
 
-    return \@col_arr;
+    return \@col_arr, $row_count;
 }
 
-sub ReadInt()
+sub WriteRowMeta($;$)
+{
+    my($fh, $params) = @_;
+
+    my $flags = 0;
+
+    if(defined $$params{deleted} && $$params{deleted})
+    {
+        $flags += 128;
+    }
+
+    print $fh pack("C",$flags );
+}
+
+sub ReadRowMeta($)
+{
+    my($fh) = @_;
+
+    my $result= {
+        deleted => 0
+    };
+
+    my $buffer;
+    read($fh, $buffer, 1);  
+    my $flags = unpack("C", $buffer);
+    
+    if($flags & (1<<7))
+    {
+        $$result{deleted} = 1;
+    }
+
+    return $result;
+}
+
+sub Select($$;$)
+{
+    my ($self, $table_name, $select_hash) = @_;
+
+    my $fh;
+
+    if(!-f $$self{db_dir} . "/$table_name")
+    {
+        die "Not Existing Table";
+    }
+    open($fh, "<", $$self{db_dir} . "/$table_name") or die $!;
+    
+    my ($arr_ref, $row_count) = ReadTableMeta($fh);
+
+    my @columns_arr = @{$arr_ref};
+    my @colnames;
+    my @result;
+
+    foreach my $column(@columns_arr) #get Colnames
+    {
+        push @colnames, $$column{name};
+    }
+    push @result, \@colnames;
+
+    while(!eof($fh))
+    {
+        my @row;
+
+        my $row_flags = ReadRowMeta($fh);
+        my $is_valid = 1;#TODO conditions!+
+        foreach my $column(@columns_arr)
+        {
+            my $val = $$column{read}->($fh);
+            #TODO do the checking
+            if(!$is_valid)
+            {
+                last;
+            }
+            push @row, $val;
+        }
+        if($is_valid && !$$row_flags{deleted})
+        {
+            push @result, \@row;
+        }
+    }
+    return \@result;
+}
+
+sub Insert($$$)#TODO insert_hash may be arr_ref
+{
+    my ($self, $table_name, $insert_hash) = @_;
+
+    my $fh;
+    if(!-f $$self{db_dir}. "/$table_name")
+    {
+        die "Not Existing Table";
+    }
+    open($fh, "+<" . $$self{db_dir} . "/$table_name") or die $!;
+
+    my ($arr_ref, $row_count) = ReadTableMeta($fh);
+   
+    seek($fh, 0, 0);
+    print $fh pack("I", $row_count + 1);
+    seek($fh, 0, 2);
+    my @columns_arr = @{$arr_ref};
+
+    my $col_count = scalar @columns_arr;
+
+    WriteRowMeta($fh);
+
+    foreach my $column(@columns_arr)
+    {
+        $$column{write}->($fh, $$insert_hash{$$column{name}});
+    }
+    
+    close($fh);
+}
+
+
+sub Update($$$)
+{
+    my ($self, $table_name, $update_hash) = @_;
+
+    my $fh;
+
+    if(!-f $$self{db_dir} . "/$table_name")
+    {
+        die "Not Existing Table";
+    }
+    open($fh, "+<", $$self{db_dir} . "/$table_name") or die $!;
+    
+    my ($arr_ref, $row_count) = ReadTableMeta($fh);
+
+    my @columns_arr = @{$arr_ref};
+    my @colnames;
+    foreach my $column(@columns_arr)
+    {
+        push @colnames, $$column{name};
+    }
+
+    my $row_counter = 0;    
+    while($row_counter < $row_count)
+    { 
+        $row_counter++;
+        local $| = 1; 
+        my @row;
+        my $is_valid = 1;
+
+        my $row_beginning_pos = tell($fh);
+
+        my $row_flags = ReadRowMeta($fh);
+        #TODO skip deleted rows
+        foreach my $column(@columns_arr)
+        {
+            my $val = $$column{read}->($fh);
+            #TODO do the checking
+            if(!$is_valid)
+            {
+                last;
+            }
+            push @row, $val;
+        }
+        
+
+        if($is_valid)
+        {
+            my $cols_count = scalar(@columns_arr);
+            my $update_row;
+
+            for(my $i = 0; $i < $cols_count; $i++)
+            {
+                $$update_row{$columns_arr[$i]{name}} = exists ($$update_hash{$columns_arr[$i]{name}}) ?  $$update_hash{$columns_arr[$i]{name}} : $row[$i]; #TODO chec    
+            }
+
+            my $next_row_position = tell($fh);
+
+            seek($fh, $row_beginning_pos, 0);
+            WriteRowMeta($fh, {deleted => 1});#updated row meta
+            seek($fh, 0, 2);          
+
+            WriteRowMeta($fh);#new row meta
+            foreach my $column(@columns_arr)
+            {
+                $$column{write}->($fh, $$update_row{$$column{name}});
+            }   
+            seek($fh, $next_row_position, 0);
+        }
+    }
+}
+
+
+
+sub ReadInt($)
 {
     my ($fh) = @_;
     my $buffer = "";
@@ -266,7 +303,8 @@ sub ReadInt()
     my $is_null = 0;
     
     read($fh, $buffer, 1);
-    if($buffer & (1 << 7))
+    my $flags = unpack("C", $buffer);
+    if($flags & (1<<7))
     {
         $is_null = 1;
     }
@@ -276,7 +314,7 @@ sub ReadInt()
     return $is_null, unpack("i", $buffer);
 }
 
-sub WriteInt()
+sub WriteInt($$)
 {
     my ($fh, $value) = @_;
 
@@ -291,16 +329,18 @@ sub WriteInt()
     print $fh pack("i", $value);
 }
 
-sub ReadString()
+sub ReadString($)
 {
     my ($fh) = @_;
     
     my $buffer ="";
 
     my $is_null = 0;
-
+    
     read($fh, $buffer, 1);
-    if($buffer & (1 << 7))
+    my $flags = unpack("C", $buffer);
+
+    if($flags & (1<<7))
     {
         $is_null = 1;
     }
@@ -313,7 +353,7 @@ sub ReadString()
     return $is_null, $buffer;
 }
 
-sub WriteString()
+sub WriteString($$)
 {
     my ($fh, $value) = @_;
     
